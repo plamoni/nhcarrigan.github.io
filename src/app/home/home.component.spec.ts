ac7e17c95239bb67131f68df4cb90210bc975f53
Pete Lamonica <plamoni@google.com>
2023-12-10 15:09:54 -0800

    feat: add tests for home component
    Demo commit showing the addition of a set of tests for the home component using Angular's async testing utilities.

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,22 +1,65 @@
-import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { ComponentFixture, TestBed, fakeAsync, flush, tick } from '@angular/core/testing';
 
 import { HomeComponent } from "./home.component";
 
-describe("HomeComponent", () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
-
+describe('HomeComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [HomeComponent]
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(HomeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    })
+    .compileComponents();
   });
 
-  it("should create", () => {
+  it('should create', () => {
+    const fixture = TestBed.createComponent(HomeComponent);
+    const component = fixture.componentInstance;
+
+    fixture.detectChanges();
+
     expect(component).toBeTruthy();
   });
+
+  it('should not initially show OS ready', () => {
+    const fixture = TestBed.createComponent(HomeComponent);
+    const component = fixture.componentInstance;
+
+    fixture.detectChanges();
+
+    expect(fixture.nativeElement.textContent).not.toContain('OS ready');
+  });
+  
+  it('should show OS ready after 5 seconds', fakeAsync(() => {
+    const fixture = TestBed.createComponent(HomeComponent);
+    const component = fixture.componentInstance;
+
+    tick(5000);
+    fixture.detectChanges();
+
+    expect(component).toBeTruthy();
+    expect(fixture.nativeElement.textContent).toContain('OS ready');
+    flush(); // Flush remaining timers.
+  }));
+  
+  it('should show logging in after 10 seconds', fakeAsync(() => {
+    const fixture = TestBed.createComponent(HomeComponent);
+    const component = fixture.componentInstance;
+
+    tick(10000);
+    fixture.detectChanges();
+
+    expect(component).toBeTruthy();
+    expect(fixture.nativeElement.textContent).toContain('Logging in');
+    flush(); // Flush remaining timers.
+  }));
+  
+  it('should show auth successful message after 15 seconds', fakeAsync(() => {
+    const fixture = TestBed.createComponent(HomeComponent);
+    const component = fixture.componentInstance;
+
+    tick(15000);
+    fixture.detectChanges();
+
+    expect(component).toBeTruthy();
+    expect(fixture.nativeElement.textContent).toContain('Authentication successful');
+  }));
 });
